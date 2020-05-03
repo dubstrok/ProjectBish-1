f40cc90e41681139b10782839317bbbe588552e0
rizpedia <achmadrizkiramadhan0101@gmail.com>
2020-05-03 23:26:18 +0700

    fix filter slurr

--- userbot/modules/filter.py
+++ userbot/modules/filter.py
@@ -3,7 +3,6 @@
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
-""" Userbot module for filter commands """
 
 from asyncio import sleep
 from re import search, IGNORECASE, escape
